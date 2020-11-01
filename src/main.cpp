0754ce7dee438388219f5880554730bf38b5afd6
Gustavo Soares <gusnsoares@gmail.com>
2020-11-01 16:08:26 -0300

    Implement constants

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include "constants.hpp"
 #include "models.hpp"
 
 int main() {
 
     Samus samus(5, 7);
 
+    std::cout << "Testando criacao de modelos" << std::endl;
     std::cout << samus.rect.x << ", " << samus.rect.y << std::endl;
+
+    std::cout << "Testando importacao de constantes" << std::endl;
+    std::cout << "Passo horizontal da Samus " << SamusMovement::horizontalStep << std::endl;
 
     return 0;
 }
