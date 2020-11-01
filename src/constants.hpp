0a900d806bb8e44e22df5fee9160cdc7f34646cc
Gustavo Soares <gusnsoares@gmail.com>
2020-11-01 16:08:26 -0300

    Implement constants

---
+++ src/constants.hpp
@@ -0,0 +1,15 @@
+#ifndef METROID_CONSTANTS_H
+#define METROID_CONSTANTS_H
+
+namespace SamusMovement {
+    const int horizontalStep = 10;
+    const int initialVerticalVelocity = 10;
+}
+
+namespace MetroidMovement {
+    const int horizontalStep = 10;
+    const int ElasticConstant = 5;
+    const int DampingConstant = 5;
+}
+
+#endif
