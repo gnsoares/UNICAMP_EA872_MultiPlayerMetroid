5da39c8133098b1564f0d86291bc69f2d86130bd
giuliasteck <giuliasteck@gmail.com>
2021-01-11 22:12:26 -0300

    Documented

--- include/receivers.hpp
+++ include/receivers.hpp
@@ -18,17 +18,11 @@ namespace Receivers {
 
     /*! \brief Receives handshakes from other players
      *
-<<<<<<< HEAD
      * \param n number of players
      * \param adresses vector containing other players' IPs
      * \param socket socket
      * \param endpoint endpoint
      * \param size integer equivalent to number of players
-=======
-     * \param vector containing other players' IPs
-     * \param socket
-     * \param endpoint
->>>>>>> c46a2cbb39d6efa273c6456bf550b47075028c26
      * \return nothing
      */
 
@@ -41,19 +35,11 @@ namespace Receivers {
     );
 
     /*! \brief Receives other players' connection infos
-<<<<<<< HEAD
      * \param adresses vector containing other player's adresses
      * \param my_ip_address strhing containing the player ip adress
      * \param socket socket
      * \param endpoint endpoint
      * \param size integer equivalent to number of players
-=======
-     * \param vector containing other player's adresses
-     * \param strhing containing the player ip adress
-     * \param socket
-     * \param endpoint
-     * \param integer equivalent to number of players
->>>>>>> c46a2cbb39d6efa273c6456bf550b47075028c26
      */
 
     void otherPlayers(
@@ -66,17 +52,10 @@ namespace Receivers {
 
     /*! \brief Receives other players' game states
      *
-<<<<<<< HEAD
      * \param state json containing other players' states
      * \param adresses other players' IP addresses
      * \param socket socket
      * \param endpoint endpoint
-=======
-     * \param json containing other players' states
-     * \param other players' IP addresses
-     * \param socket
-     * \param endpoint
->>>>>>> c46a2cbb39d6efa273c6456bf550b47075028c26
      * \return nothing
      */
     void state(
